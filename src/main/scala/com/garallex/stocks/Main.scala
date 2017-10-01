7b049378339ab1fdcfc20fd334bb7329a7d27bcf
Oleksandr Iskhakov <oleksandr.iskhakov@redmart.com>
2017-10-01 13:57:47 +0800

    LongTermGrowthProspects

--- src/main/scala/com/garallex/stocks/Main.scala
+++ src/main/scala/com/garallex/stocks/Main.scala
@@ -9,6 +9,8 @@ import org.apache.http.impl.client.HttpClients
 import org.apache.http.message.BasicNameValuePair
 import org.apache.http.util.EntityUtils
 
+import scala.collection.SortedMap
+
 
 object Main {
   def grahamPreFiltered(stock: Stock): Option[Boolean] =
@@ -28,34 +30,59 @@ object Main {
     }
 
   def main(args: Array[String]): Unit = {
-//    val httpClient = HttpClients.createDefault
-//    val postRequest = new HttpPost("https://www.morningstar.com/members/login.html")
-//    val nvps = new util.ArrayList[NameValuePair]
-//    nvps.add(new BasicNameValuePair("uim-uEmail-input", "alexander.iskhakov@gmail.com"))
-//    nvps.add(new BasicNameValuePair("uim-uPassword-input", ""))
-//    nvps.add(new BasicNameValuePair("remember_me", "true"))
-//    postRequest.setEntity(new UrlEncodedFormEntity(nvps))
-//    val response = httpClient.execute(postRequest)
-//    try {
-//      System.out.println(response.getStatusLine)
-////      val entity2 = response.getEntity
-//      // do something useful with the response body
-//      // and ensure it is fully consumed
-////      EntityUtils.consume(entity2)
-//    } finally response.close()
+//    val xy = List((100, 50), (110, 60), (120, 70))
+//
+//    val xyy = xy.scanLeft((0, 100)) { case ((prev1, prev2), (bucket, avail)) =>
+//      val toCurrentBucket = Math.min(avail, prev2)
+//      val passFurther = prev2 - toCurrentBucket
+//      (bucket + toCurrentBucket, avail - toCurrentBucket)
+//    }
+//    println(xyy)
+//
+//    val lx = SortedMap(1 -> "a", 2 -> "b", 3 -> "c", 4 -> "d", 5 -> "e", 6 -> "f", 7 -> "g")
+//    val ly = SortedMap(2 -> "u", 3 -> "v", 6 -> "w", 7 -> "x")
+//
+//    val itx = lx.iteratorFrom(2)
+//    val ity = ly.iteratorFrom(2)
+//
+//    val itc = itx zip ity
+//
+//    val res = for {(x, y) <- itc
+//    //      println(s"$x - $y")
+//
+//    } yield (x, y)
+//
+//    println(res.isEmpty)
+//    for (xx <- res) println(xx)
+//
+//    return
+    //    val httpClient = HttpClients.createDefault
+    //    val postRequest = new HttpPost("https://www.morningstar.com/members/login.html")
+    //    val nvps = new util.ArrayList[NameValuePair]
+    //    nvps.add(new BasicNameValuePair("uim-uEmail-input", "alexander.iskhakov@gmail.com"))
+    //    nvps.add(new BasicNameValuePair("uim-uPassword-input", ""))
+    //    nvps.add(new BasicNameValuePair("remember_me", "true"))
+    //    postRequest.setEntity(new UrlEncodedFormEntity(nvps))
+    //    val response = httpClient.execute(postRequest)
+    //    try {
+    //      System.out.println(response.getStatusLine)
+    ////      val entity2 = response.getEntity
+    //      // do something useful with the response body
+    //      // and ensure it is fully consumed
+    ////      EntityUtils.consume(entity2)
+    //    } finally response.close()
 
     while (true) {
       val ticker = scala.io.StdIn.readLine("Ticker:")
       if (ticker == null || ticker == "") return
 
-//      val stock1 = new StockBuilder(ticker.toUpperCase).build()
-//      val stock = if (ticker.toUpperCase() == "BABA") stock1.copy(beta = Some(BigDecimal("1.16"))) else stock1
+      //      val stock1 = new StockBuilder(ticker.toUpperCase).build()
+      //      val stock = if (ticker.toUpperCase() == "BABA") stock1.copy(beta = Some(BigDecimal("1.16"))) else stock1
 
       val stock = new StockBuilder(ticker.toUpperCase).build()
 
       println(new StockToString(stock))
     }
-
     //    val doc = new edu.stanford.nlp.simple.Document("I live with a Vietnam Vet who served in the late 1960s with 1st Cav. Medivac. During service he earned two Purple Hearts, the Distinguished Flying Cross, and the Air Medal. Since WE WERE SOLDIERS concerns the 1st Cav., Randy wanted to see it. I reluctantly agreed; I am not partial to war films and I dislike Mel Gibson, and Randy is very hard on Vietnam War films. He dismisses PLATOON as a Hollywood 8x10 glossy; says APOCALYPSE NOW is an interesting movie that captures the paranoia, but all the technical details are wrong; and describes DEER HUNTER as excellent in its depiction of the strangeness of coming home but so full of plot holes that he can hardly endure it. And about one and all he says: \"It wasn't like that.\"\n\nHe was silent through the film, and when we left the theatre I asked what he thought. He said, \"They finally got it. That's what it was like. All the details are right. The actors were just like the men I knew. They looked like that and they talked like that. And the army wives too, they really were like that, at least every one I ever knew.\" The he was silent for a long time. At last he said, \"You remember the scene where the guy tries to pick up a burn victim by the legs and all the skin slides off? Something like that happened to me once. It was at a helicopter crash. I went to pick him up and all the skin just slid right off. It looked just like that, too. I've never told any one about it.\" In most respects WE WERE SOLDIERS is a war movie plain and simple. There are several moments when the film relates the war to the politics and social movements that swirled about it, and the near destruction of the 1st. Cav.'s 7th Battalion at Ia Drang clearly arises from the top brass' foolish decision to send the 7th into an obvious ambush--but the film is not so much interested in what was going on at home or at the army's top as it is in what was actually occurring on the ground. And in this it is extremely meticulous, detailed, and often horrifically successful. Neither Randy nor I--nor any one in the theatre I could see--was bored by or dismissive of the film. It grabs you and it grabs you hard, and I can easily say that it is one of the finest war movies I have ever seen, far superior to the likes of SAVING PRIVATE RYAN, which seems quite tame in comparison.\n\nPerhaps the single most impressive thing about the film is that it never casts its characters in a heroic light; they are simply soldiers who have been sent to do a job, and they do it knowing the risks, and they do it well in spite of the odds. Mel Gibson, although I generally despise him as both an actor and a human being, is very, very good as commanding officer Hal Moore, and he is equaled by Sam Elliot, Greg Kinnear, Chris Klein, and every other actor on the battlefield. The supporting female cast, seen early in the film and in shorter scenes showing the home front as the battle rages, is also particularly fine, with Julie Moore able to convey in glance what most actresses could not communicate in five pages of dialogue. The script, direction, cinematography, and special effects are sharp, fast, and possess a \"you are there\" quality that is very powerful.\n\nI myself had a criticism; there were points in the film when I found the use of a very modernistic, new-agey piece of music to be intrusive and out of place. And we both felt that a scene near the end of the movie, when a Vietnamese commander comments on the battle, to be improbable and faintly absurd. But these are nit-picky quibbles. WE WERE SOLDIERS is a damn fine movie. I'll give Randy, who served two tours of duty in Vietnam, the last word: \"It may not be 'the' Vietnam movie. I don't think there could ever be 'the' Vietnam movie. But they pretty much get everything right. That's how it looked and sounded, and that's what I saw, and this is the best movie about Vietnam I've ever seen.\" Gary F. Taylor, aka GFT, Amazon Reviewer")
     //    val sentences = doc.sentences()
     //    import scala.collection.JavaConverters._
